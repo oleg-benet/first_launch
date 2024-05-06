cb80d7ad911f7a1563ab8d85ef65cddc75640547
Oleg <benetal@yandex.ru>
2024-05-06 19:27:01 +0700

    Remove NewsPaper

--- NewsPaper/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-# Импортируем созданное нами представление
-from .views import PostList, PostDetail
-
-urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:id>', PostDetail.as_view()),
-]
